a730a092abe75f38fe491da6a9194c894177c1a0
Kevin Hou <k@exafunction.com>
2023-12-08 17:48:31 -0800

    Basic Monaco editor embedded

--- src/components/CodeiumEditor/CodeiumEditor.tsx
+++ src/components/CodeiumEditor/CodeiumEditor.tsx
@@ -1,7 +1,47 @@
-import React from "react";
+"use client";
 
-export type CodeiumEditorProps = {};
+import React, { useCallback, useMemo, useRef } from "react";
+import Editor, { EditorProps, Monaco } from "@monaco-editor/react";
+import * as monaco from "monaco-editor/esm/vs/editor/editor.api";
 
-export const CodeiumEditor: React.FC<CodeiumEditorProps> = () => {
-  return <div>CodeiumEditor</div>;
+export type CodeiumEditorProps = {
+  editorProps: EditorProps;
+};
+
+/**
+ * Code editor that enables Codeium AI suggestions in the editor.
+ */
+export const CodeiumEditor: React.FC<CodeiumEditorProps> = ({
+  editorProps,
+}) => {
+  const editorRef = useRef<monaco.editor.IStandaloneCodeEditor | null>(null);
+
+  const handleEditorDidMount = (
+    editor: monaco.editor.IStandaloneCodeEditor,
+    monaco: Monaco
+  ) => {
+    editorRef.current = editor;
+
+    // Pass the editor instance to the user defined onMount prop.
+    if (editorProps.onMount) {
+      editorProps.onMount(editor, monaco);
+    }
+  };
+
+  let defaultLanguageProps: EditorProps = {};
+  if (!editorProps.defaultLanguage) {
+    defaultLanguageProps = {
+      defaultLanguage: "javascript",
+      defaultValue:
+        "// Welcome to Codeium!\n// Press Enter and use Tab to accept AI suggestions. Here's an example:\n\n// Write me a function that adds two numbers.",
+    };
+  }
+
+  return (
+    <Editor
+      {...defaultLanguageProps}
+      {...editorProps}
+      onMount={handleEditorDidMount}
+    />
+  );
 };
