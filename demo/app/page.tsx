dcfc26c72280dca710a35ce449422211380841ca
Andy Zhang <andzhng@gmail.com>
2024-01-30 18:26:32 -0800

    Add event handler for onAutocomplete

--- demo/app/page.tsx
+++ demo/app/page.tsx
@@ -1,12 +1,12 @@
 "use client"
-import Image from 'next/image'
+import { useState } from 'react';
 import styles from './page.module.css'
 import { CodeiumEditor } from "@codeium/react-code-editor";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <CodeiumEditor language="python" />
+      <CodeiumEditor language="python"/>
     </main>
   )
 }
