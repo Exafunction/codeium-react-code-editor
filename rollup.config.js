30171ff04a8f85a00e9a2c4aed0fdc1f2f83e6be
Andy Zhang <andzhng@gmail.com>
2024-01-31 10:53:28 -0800

    Export package as cjs instead of esm

--- rollup.config.js
+++ rollup.config.js
@@ -33,6 +33,6 @@ export default [
   {
     input: "dist/esm/types/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
-    plugins: [dts()],
+    plugins: [dts.default()],
   },
 ];
